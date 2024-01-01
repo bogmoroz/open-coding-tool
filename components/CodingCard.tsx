4684e1d52b95c2d3f2bfd58d45619aa1ac33ed53
Bogdan Moroz <bogdan.moroz@observis.fi>
2024-01-02 00:59:31 +0200

    coding card titles now link to sources

--- components/CodingCard.tsx
+++ components/CodingCard.tsx
@@ -3,11 +3,13 @@ import {
   Card,
   CardActions,
   CardContent,
-  Typography
+  Typography,
+  Link as MuiLink
 } from '@mui/material';
 import { Coding, Source } from '../types';
 import { useEffect, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 interface CodingCardProps {
   source?: Source;
@@ -71,9 +73,32 @@ const CodingCard: React.FC<CodingCardProps> = (props) => {
     >
       <CardContent>
         {props.source && (
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <MuiLink
+            href={`source/${props.source.sourceNumber}/`}
+            target="_blank"
+            rel="noreferrer"
+          >
             {props.source.sourceNumber} {props.source.publicationTitle}
-          </Typography>
+          </MuiLink>
+          // <MuiLink>
+          //   <Link
+          //     href={{
+          //       pathname: `source/${props.source.sourceNumber}/`
+          //     }}
+          //     legacyBehavior
+          //     rel="noopener noreferrer"
+          //     target="_blank"
+          //     passHref
+          //   >
+          //     <Typography
+          //       sx={{ fontSize: 14 }}
+          //       color="text.secondary"
+          //       gutterBottom
+          //     >
+          //       {props.source.sourceNumber} {props.source.publicationTitle}
+          //     </Typography>
+          //   </Link>
+          // </MuiLink>
         )}
         {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Code name
